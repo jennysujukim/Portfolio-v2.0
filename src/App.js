b5691f06ee9acceeb2963aea53bd6685240e5bab
jennysujukim <jennysujukim@gmail.com>
2023-07-13 01:25:00 -0700

    test work page lazy loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ScrollToTop } from './hooks/useScrollToTop';
 
@@ -7,15 +8,16 @@ import './App.scss';
 // components
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Terminal from './components/Terminal';
-
+import Terminal from './components/Terminal'
+import PageLoader from './components/PageLoader'
 
 
 // pages
 import Home from './pages/home'
 import About from './pages/about'
-import Work from './pages/work'
 import NotFound from './pages/notfound'
+const LazyWork = lazy(() => import('./pages/work'))
+
 
 
 function App() {
@@ -28,7 +30,7 @@ function App() {
 <Routes>
   <Route path="/" element={<Home />}/>
   <Route path="/about" element={<About />}/>
-  <Route path="/work/:id" element={<Work />}/>
+  <Route path="/work/:id" element={<Suspense fallback={<PageLoader />}><LazyWork /></Suspense>}/>
   <Route path="/terminal" element={<Terminal />}/>
   <Route path="*" element={<NotFound />}/>
 </Routes>
--- src/index.js
+++ src/index.js
@@ -8,16 +8,13 @@ import './index.scss';
 // components
 import App from './App';
 import { ScrollToTop } from './hooks/useScrollToTop';
-import PageLoader from './components/PageLoader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
         <ScrollToTop />
-        <React.Suspense fallback={<PageLoader />}>
           <App />
-        </React.Suspense>
     </React.StrictMode>
   </BrowserRouter>
 );
